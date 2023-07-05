81caa836cf8d973d4342e4f41c9a1f0779e08163
fikazi <azizifikrul01@gmail.com>
2023-07-06 01:50:41 +0700

    test

--- content.js
+++ content.js
@@ -1,13 +1,17 @@
+
 async function getData(){
     const data = await fetch('data.json')
-    .then(res => res.json ())
+    .then((res) => res.json ())
+    console.log('test')
     return data
   }
   
 const run = async ()=>{
 const contenData = await getData()
+console.log(contenData)
+console.log('testt')
 
-const content = document.querySelector('section#content')
+const content = document.getElementById('section#content')
 
 for (let i = 0; i <contentData.lenght; i++) {
     content.innerHTML += 
--- gallery.js
+++ gallery.js
@@ -1,3 +1,14 @@
 function masuk() {
     alert('testing')
 }
+
+const variableA = 10
+let variableB = 20
+variableB = 1
+
+const data = [
+  'apel',
+  'jeruk',
+  'mangga',
+  'pepaya'
+]
